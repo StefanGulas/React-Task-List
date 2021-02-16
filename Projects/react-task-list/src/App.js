c0c8478962ddba40eec334e4e17efcd187bd06f1
stefan.gulas <stefan.gulas@gmail.com>
2021-02-16 10:59:55 +0100

    individual task implemented

--- Projects/react-task-list/src/App.js
+++ Projects/react-task-list/src/App.js
@@ -6,19 +6,19 @@ function App() {
   const [tasks, setTasks] = useState([
     {
         id: 1,
-        text: 'Doctors Appointment',
+        text: 'Arztbesuch',
         day: 'Feb 5th at 2:30pm',
         reminder: true,
     },
     {
         id: 2,
-        text: 'Meeting at School',
+        text: 'Elternsprechtag',
         day: 'Feb 6th at 1:30pm',
         reminder: true,
     }, 
     {
         id: 3,
-        text: 'Food Shopping',
+        text: 'Essen kaufen',
         day: 'Feb 5th at 2:30pm',
         reminder: false,
     },
